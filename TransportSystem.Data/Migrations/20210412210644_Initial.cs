d2bea9dcb6d777353df7332d1f6c4e8944d3e6ea
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2021-04-12 22:19:44 +0100

    Fixed Migration Error

---
+++ TransportSystem.Data/Migrations/20210412210644_Initial.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TransportSystem.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "buses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    RowVersion = table.Column<DateTime>(rowVersion: true, nullable: true)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn),
+                    BusNumber = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Capacity = table.Column<string>(type: "(50)", nullable: false),
+                    Model = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Speed = table.Column<string>(type: "varchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_buses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "passenger",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    RowVersion = table.Column<DateTime>(rowVersion: true, nullable: true)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn),
+                    FirstName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    LastName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Email = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Address = table.Column<string>(type: "varchar(255)", nullable: true),
+                    NextOfKin = table.Column<string>(type: "varchar(50)", nullable: true),
+                    PasswordHash = table.Column<string>(type: "varchar(750)", nullable: false),
+                    HashSalt = table.Column<string>(type: "varchar(700)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_passenger", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "trips",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    RowVersion = table.Column<DateTime>(rowVersion: true, nullable: true)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn),
+                    BusId = table.Column<Guid>(nullable: false),
+                    Date = table.Column<DateTime>(type: "dateTime(50)", nullable: false),
+                    TakeOff = table.Column<string>(type: "(50)", nullable: false),
+                    Destination = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DepatureTime = table.Column<DateTime>(type: "dateTime(50)", nullable: false),
+                    Arrival = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_trips", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_trips_buses_BusId",
+                        column: x => x.BusId,
+                        principalTable: "buses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bookings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    RowVersion = table.Column<DateTime>(rowVersion: true, nullable: true)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn),
+                    TripId = table.Column<Guid>(nullable: false),
+                    BookingNumber = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Price = table.Column<double>(type: "double(50)", nullable: false),
+                    PassengerId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_bookings_passenger_PassengerId",
+                        column: x => x.PassengerId,
+                        principalTable: "passenger",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_bookings_trips_TripId",
+                        column: x => x.TripId,
+                        principalTable: "trips",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_PassengerId",
+                table: "bookings",
+                column: "PassengerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_TripId",
+                table: "bookings",
+                column: "TripId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_buses_BusNumber",
+                table: "buses",
+                column: "BusNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_passenger_Email",
+                table: "passenger",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_trips_BusId",
+                table: "trips",
+                column: "BusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bookings");
+
+            migrationBuilder.DropTable(
+                name: "passenger");
+
+            migrationBuilder.DropTable(
+                name: "trips");
+
+            migrationBuilder.DropTable(
+                name: "buses");
+        }
+    }
+}
